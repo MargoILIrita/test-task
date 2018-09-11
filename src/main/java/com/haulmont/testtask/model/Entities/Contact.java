fdddbdfa97353387605334ef04d9b7dbdc045542
Margarita <rita96pol@gmail.com>
2018-09-12 00:35:22 +0400

    всё никак свой скрипт не запущу

--- src/main/java/com/haulmont/testtask/model/Entities/Contact.java
+++ src/main/java/com/haulmont/testtask/model/Entities/Contact.java
@@ -1,13 +1,18 @@
 package com.haulmont.testtask.model.Entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
 
 @Entity
 public class Contact {
+    @Id
+    @GeneratedValue
     private long id;
 
-
+    @Column(nullable = false)
     private String name;
 
     private String number;
@@ -38,7 +43,6 @@ public class Contact {
         this.email = email;
     }
 
-    @Id
     public long getId() {
         return id;
     }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into contact (id, name, email, phone) values (20, 'a', 'a', 'a');
+insert into contact (id, name, email, phone) values (21, 'b', 'b', 'b');
+insert into contact (id, name, email, phone) values (22, 'c', 'c', 'c');
